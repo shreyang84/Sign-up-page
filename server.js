70a094e03a4a4a1fe8beedb18c0edf99d0f95d6a
shreyang84 <shreyang.vadhvana@gmail.com>
2017-06-16 11:49:19 -0400

    adding new file

---
+++ server.js
@@ -0,0 +1,12 @@
+var port = 85,
+express = require('express'),
+app = express().use(express.static(__dirname + '/')),
+http = require('http').Server(app);
+
+app.use('/', function(req, res){
+    res.sendFile(__dirname + '/form.html');
+});
+
+http.listen(port, function(){
+    console.log("Node server listening on port " + port);
+});
